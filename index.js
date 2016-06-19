e6492e4bb03c04f6be04af6975c90a24bfc28c90
uniibu <unibtc@gmail.com>
2016-06-20 03:51:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const nodePath = require('path');
 // 3rd
 const nodeIp = require('ipaddr.js');
 
-const listPath = nodePath.join(__dirname, 'ips.json');
-const cloudflareIps = JSON.parse(fs.readFileSync(listPath, 'utf8'));
+const listPathv4 = nodePath.join(__dirname, 'ipsv4.json');
+const listPathv6 = nodePath.join(__dirname, 'ipsv6.json');
+const cloudflareIpsv4 = JSON.parse(fs.readFileSync(listPathv4, 'utf8'));
+const cloudflareIpsv6 = JSON.parse(fs.readFileSync(listPathv6, 'utf8'))
 
 module.exports = function (testIp) {
   if (!testIp) return false;
@@ -14,7 +16,13 @@ module.exports = function (testIp) {
  // if (nodeIp.isPrivate(testIp)) return false;
   if (!nodeIp.IPv4.isValid(testIp) && !nodeIp.IPv6.isValid(testIp)) return false;
 testIp = nodeIp.process(testIp);
-  return cloudflareIps.some((cfIp) => {
+if(testIp.kind() === 'ipv4'){
+  return cloudflareIpsv4.some((cfIp) => {
       return testIp.match(nodeIp.parseCIDR(cfIp));
   });
+}else if (testIp.kind() === 'ipv6'){
+ return cloudflareIpsv6.some((cfIp) => {
+      return testIp.match(nodeIp.parseCIDR(cfIp));
+  });
+}
 };
