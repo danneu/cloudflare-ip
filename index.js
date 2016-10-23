3bfd9fa8a43377e3a533264ed2a520f37f6546c0
Dan Neumann <danrodneu@gmail.com>
2016-10-23 04:31:30 -0500

    Swap ip address parser

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-'use strict';
+'use strict'
 // Node
-const fs = require('fs');
-const nodePath = require('path');
+const fs = require('fs')
+const nodePath = require('path')
 // 3rd
-const nodeIp = require('ip');
+const {Address4, Address6} = require('ip-address')
 
-const listPath = nodePath.join(__dirname, 'ips.json');
-const cloudflareIps = JSON.parse(fs.readFileSync(listPath, 'utf8'));
+const listPath = nodePath.join(__dirname, 'ips.json')
+const cloudflareIps = JSON.parse(fs.readFileSync(listPath, 'utf8'))
+  .map(intoAddress)
 
-module.exports = function (testIp) {
-  if (!testIp) return false;
-  if (typeof testIp === 'string' && testIp.trim().length === 0) return false;
-  if (nodeIp.isPrivate(testIp)) return false;
-  if (!nodeIp.isV4Format(testIp) && !nodeIp.isV6Format(testIp)) return false;
-  return cloudflareIps.some((cfIp) => {
-    return nodeIp.cidrSubnet(cfIp).contains(testIp);
-  });
-};
+// returns undefined | Address4 | Address6
+function intoAddress (str) {
+  let ip = new Address6(str)
+  if (ip.v4 && !ip.valid) {
+    ip = new Address4(str)
+  }
+  if (!ip.valid) return
+  return ip
+}
+
+// returns bool
+module.exports = function (testIpString) {
+  if (!testIpString) return false
+  const testIp = intoAddress(testIpString)
+  if (!testIp) return false
+  return cloudflareIps.some((cf) => testIp.isInSubnet(cf))
+}
