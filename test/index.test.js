45a784de24860dfebf9f6946335c33bd426290dd
Dan Neumann <danrodneu@gmail.com>
2016-10-23 04:51:18 -0500

    Update tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,40 +1,41 @@
-'use strict';
+'use strict'
 // 3rd
-const assert = require('chai').assert;
+const assert = require('chai').assert
 // 1st
-const cloudflareIp = require('..');
+const cloudflareIp = require('..')
 
 describe('falsey values', () => {
   it('is false', () => {
-    assert.isFalse(cloudflareIp());
-    assert.isFalse(cloudflareIp(''));
-    assert.isFalse(cloudflareIp(0));
+    assert.isFalse(cloudflareIp())
+    assert.isFalse(cloudflareIp(''))
+    assert.isFalse(cloudflareIp(0))
   })
 })
 
 describe('garbage string values', () => {
   it('is false', () => {
-    assert.isFalse(cloudflareIp('chicken'));
+    assert.isFalse(cloudflareIp('chicken'))
   })
 })
 
 describe('non-cloudflare IPs', () => {
   it('is false', () => {
-    assert.isFalse(cloudflareIp('66.249.66.1'));
-    assert.isFalse(cloudflareIp('1.1.1.1'));
+    assert.isFalse(cloudflareIp('66.249.66.1'))
+    assert.isFalse(cloudflareIp('1.1.1.1'))
   })
 })
 
 describe('localhost', () => {
   it('is false', () => {
-    assert.isFalse(cloudflareIp('127.0.0.1'));
-    assert.isFalse(cloudflareIp('::1'));
+    assert.isFalse(cloudflareIp('127.0.0.1'))
+    assert.isFalse(cloudflareIp('::1'))
   })
 })
 
 describe('cloudflare IPs', () => {
   it('is true', () => {
-    assert.isTrue(cloudflareIp('103.21.244.0'));
-    assert.isTrue(cloudflareIp('2400:cb00:0000::0000'));
+    assert.isTrue(cloudflareIp('103.21.244.0'))
+    assert.isTrue(cloudflareIp('2400:cb00:0000::0000'))
+    assert.isTrue(cloudflareIp('108.162.212.58'))
   })
 })
